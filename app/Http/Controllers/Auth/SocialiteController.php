60135b97b7ee3bb8c5d722bccbc9870683ce7027
fazaimron27 <cincaumyself@gmail.com>
2019-10-01 21:00:48 +0700

    create function login

--- app/Http/Controllers/Auth/SocialiteController.php
+++ app/Http/Controllers/Auth/SocialiteController.php
@@ -4,12 +4,28 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Socialite;
 
 class SocialiteController extends Controller
 {
     public function redirectToProvider($provider)
-    { }
+    {
+        return Socialite::driver($provider)->redirect();
+    }
 
     public function handleProviderCallback($provider)
-    { }
+    {
+        try {
+            $user = Socialite::driver($provider)->user();
+        } catch (Exception $e) {
+            return redirect('/login');
+        }
+
+        $authUser = $this->findOrCreateUser($user, $provider);
+
+        Auth::login($authUser, true);
+
+        return redirect('/home');
+    }
 }
