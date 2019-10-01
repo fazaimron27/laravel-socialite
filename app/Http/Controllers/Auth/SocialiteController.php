c84eef054ed7ed1835041973fb0794efdb0ca0f3
fazaimron27 <cincaumyself@gmail.com>
2019-10-01 21:14:24 +0700

    create function validation user

--- app/Http/Controllers/Auth/SocialiteController.php
+++ app/Http/Controllers/Auth/SocialiteController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\Http\Controllers\Controller;
+use App\SocialAccount;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use App\User;
 use Illuminate\Support\Facades\Auth;
-use Socialite;
+use Laravel\Socialite\Facades\Socialite;
 
 class SocialiteController extends Controller
 {
@@ -27,5 +29,32 @@ class SocialiteController extends Controller
         Auth::login($authUser, true);
 
         return redirect('/home');
+    }
+
+    public function findOrCreateUser($socialUser, $provider)
+    {
+        $socialAccount = SocialAccount::where('provider_id', $socialUser->getId())
+            ->where('provider_name', $provider)
+            ->first();
+
+        if ($socialAccount) {
+            return $socialAccount->user;
+        } else {
+            $user = User::where('email' . $socialUser->getEmail())->first();
+
+            if (!$user) {
+                $user = User::create([
+                    'name' => $socialUser->getName(),
+                    'email' => $socialUser->getEmail()
+                ]);
+            }
+
+            $user->SocialAccounts()->create([
+                'provider_id' => $socialUser->getId(),
+                'provider_name' => $provider
+            ]);
+
+            return $user;
+        }
     }
 }
